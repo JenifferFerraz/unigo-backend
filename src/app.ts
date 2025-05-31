64132bf55ef7684088726555e6ee9438d92f25e7
Jeniffer Ferraz Pires <122577117+JenifferFerraz@users.noreply.github.com>
2025-05-31 19:43:16 -0300

    fix:payload

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ import { notificationRoutes } from './routes/notification.routes';
 
 const app = express();
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
   origin: (origin, callback) => {
