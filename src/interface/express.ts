b239d5d6d42c865a0c168099d0da6c4a423d7e02
Jeniffer Ferraz Pires <122577117+JenifferFerraz@users.noreply.github.com>
2025-08-26 10:42:32 -0300

    fix: corrigir formatação do namespace Express no arquivo express.ts

--- src/interface/express.ts
+++ src/interface/express.ts
@@ -1,11 +1,12 @@
-import { Request } from 'express';
 import { User } from '../entities/User';
 
 declare global {
-    namespace Express {
-        interface Request {
-            authMail?: string;
-            user?: User;
-        }
+  namespace Express {
+    interface Request {
+      authMail?: string;
+      user?: User;
     }
+  }
 }
+
+export {};
